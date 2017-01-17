f87040ccf790450df97703b430d4d7380aeb1fa4
Bob Lee <Bob@Bobs-MacBook-Pro.local>
2017-01-17 11:56:37 +0900

    Swift 3

---
+++ Protocol Oriented Programming View with Bob/Shakeable.swift
@@ -0,0 +1,9 @@
+//
+//  Shakeable.swift
+//  Protocol Oriented Programming View with Bob
+//
+//  Created by Bob Lee on 1/16/17.
+//  Copyright © 2017 BobtheDeveloper. All rights reserved.
+//
+
+import Foundation
